38a29a82d494a90b3fd1080bf45c26f8a9fd882f
cebka@PC-cebka <cebka@PC-cebka>
2007-09-20 20:00:07 +0400

    Add skeleton for spf handling

---
+++ spf.c
@@ -0,0 +1,19 @@
+/*
+ * $Id$
+ */
+
+
+#include <sys/types.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <db.h>
+
+#include "cfg_file.h"
+#include "spf.h"
+
+int read_spf_map (struct config_file *cfg, const char *path)
+{
+	/* Knob */
+	return 1;
+}
---
+++ spf.h
@@ -0,0 +1,12 @@
+/*
+ * $Id$
+ */
+
+#ifndef SPF_H
+#define SPF_H
+
+#include "cfg_file.h"
+
+int read_spf_map (struct config_file *, const char *);
+
+#endif /* SPF_H */
