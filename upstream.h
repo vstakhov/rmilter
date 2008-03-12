0faf9e55b477933531f4de9f62f677033e3ceae8
cebka@cebka-laptop <cebka@cebka-laptop>
2008-03-12 18:41:55 +0300

    * Changes to upstream:  - Migrate to rwlock to improve locking in multi-threaded applications  - Change hash function to crc32 and improve hashing in case if we mark some upstream as broken  - Add support of ketama consistent hash

--- upstream.h
+++ upstream.h
@@ -9,11 +9,14 @@ struct upstream {
 	unsigned char dead;
 	unsigned char priority;
 	int16_t weight;
+	uint32_t *ketama_points;
+	size_t ketama_points_size;
 };
 
 void upstream_fail (struct upstream *up, time_t now);
 void upstream_ok (struct upstream *up, time_t now);
 void revive_all_upstreams (void *ups, size_t members, size_t msize);
+int upstream_ketama_add (struct upstream *up, char *up_key, size_t keylen, size_t keypoints);
 
 struct upstream* get_random_upstream   (void *ups, size_t members, size_t msize, 
 										time_t now, time_t error_timeout, 
@@ -28,6 +31,9 @@ struct upstream* get_upstream_round_robin (void *ups, size_t members, size_t msi
 										time_t now, time_t error_timeout, 
 										time_t revive_timeout, size_t max_errors);
 
+struct upstream* get_upstream_by_hash_ketama (void *ups, size_t members, size_t msize, time_t now, 
+										time_t error_timeout, time_t revive_timeout, size_t max_errors,
+										char *key, size_t keylen);
 
 
 #endif /* UPSTREAM_H */
