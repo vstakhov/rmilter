8a4191f85aff29cc86475eb684001b7ea803d9ec
Vsevolod Stakhov <vsevolod@rambler-co.ru>
2012-12-24 17:55:58 +0400

    Add debian folder.

---
+++ debian/rmilter.default.ex
@@ -0,0 +1,10 @@
+# Defaults for rmilter initscript
+# sourced by /etc/init.d/rmilter
+# installed at /etc/default/rmilter by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
