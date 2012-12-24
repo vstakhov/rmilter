7a4348b4c31c0deca72975a9e724f214229cd11b
Vsevolod Stakhov <vsevolod@rambler-co.ru>
2012-12-24 18:29:24 +0400

    Licenses, debian policy, fix manpage.

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(rmilter):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="rmilter" command="/usr/bin/rmilter"
--- debian/rmilter.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the rmilter package
-#
-0 4	* * *	root	[ -x /usr/bin/rmilter_maintenance ] && /usr/bin/rmilter_maintenance
