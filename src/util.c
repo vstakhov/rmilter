b38ec88109343421e6a6f8285881be723e69f02a
Vsevolod Stakhov <vsevolod@highsecure.ru>
2015-11-16 09:36:06 +0000

    Add compatibility with compilers with no bswap64 builtin

--- src/util.c
+++ src/util.c
@@ -314,6 +314,47 @@ rmilter_pidfile_remove(rmilter_pidfh_t *pfh)
 	return (_rmilter_pidfile_remove (pfh, 1));
 }
 
+/*
+ * Written by Manuel Bouyer <bouyer@NetBSD.org>.
+ * Public domain.
+ */
+static uint32_t
+bswap32 (uint32_t x)
+{
+	return ((x << 24) & 0xff000000) |
+			((x << 8) & 0x00ff0000) |
+			((x >> 8) & 0x0000ff00) |
+			((x >> 24) & 0x000000ff);
+}
+
+static uint64_t
+bswap64 (uint64_t x)
+{
+#ifdef _LP64
+	/*
+	 * Assume we have wide enough registers to do it without touching
+	 * memory.
+	 */
+	return ((x << 56) & 0xff00000000000000UL) |
+			((x << 40) & 0x00ff000000000000UL) |
+			((x << 24) & 0x0000ff0000000000UL) |
+			((x << 8) & 0x000000ff00000000UL) |
+			((x >> 8) & 0x00000000ff000000UL) |
+			((x >> 24) & 0x0000000000ff0000UL) |
+			((x >> 40) & 0x000000000000ff00UL) |
+			((x >> 56) & 0x00000000000000ffUL);
+#else
+	/*
+	 * Split the operation in two 32bit steps.
+	 */
+	uint32_t tl, th;
+
+	th = bswap32((uint32_t)(x & 0x00000000ffffffffULL));
+	tl = bswap32((uint32_t)((x >> 32) & 0x00000000ffffffffULL));
+	return ((uint64_t)th << 32) | tl;
+#endif
+}
+
 static char *
 rmilter_encode_base64_common (const u_char *in, size_t inlen, int str_len,
 		size_t *outlen, int fold)
@@ -349,8 +390,7 @@ while (0)
 	while (inlen > 6) {
 		n = *(uint64_t *) in;
 #if BYTE_ORDER == LITTLE_ENDIAN
-		/* XXX: GCCism */
-		n = __builtin_bswap64 (n);
+		n = bswap64 (n);
 #endif
 		if (str_len <= 0 || cols <= str_len - 8) {
 			*o++ = b64_enc[(n >> 58) & 0x3F];
