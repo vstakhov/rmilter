0836f472aef096f9d729fd6b5192f2a197b2a730
Vsevolod Stakhov <vsevolod@highsecure.ru>
2015-11-16 17:16:08 +0000

    Detect if bswap32/64 are macros

--- src/util.c
+++ src/util.c
@@ -318,42 +318,46 @@ rmilter_pidfile_remove(rmilter_pidfh_t *pfh)
  * Written by Manuel Bouyer <bouyer@NetBSD.org>.
  * Public domain.
  */
-static uint32_t
-bswap32 (uint32_t x)
-{
-	return ((x << 24) & 0xff000000) |
-			((x << 8) & 0x00ff0000) |
-			((x >> 8) & 0x0000ff00) |
-			((x >> 24) & 0x000000ff);
-}
-
-static uint64_t
-bswap64 (uint64_t x)
-{
-#ifdef _LP64
-	/*
-	 * Assume we have wide enough registers to do it without touching
-	 * memory.
-	 */
-	return ((x << 56) & 0xff00000000000000UL) |
-			((x << 40) & 0x00ff000000000000UL) |
-			((x << 24) & 0x0000ff0000000000UL) |
-			((x << 8) & 0x000000ff00000000UL) |
-			((x >> 8) & 0x00000000ff000000UL) |
-			((x >> 24) & 0x0000000000ff0000UL) |
-			((x >> 40) & 0x000000000000ff00UL) |
-			((x >> 56) & 0x00000000000000ffUL);
-#else
-	/*
-	 * Split the operation in two 32bit steps.
-	 */
-	uint32_t tl, th;
-
-	th = bswap32((uint32_t)(x & 0x00000000ffffffffULL));
-	tl = bswap32((uint32_t)((x >> 32) & 0x00000000ffffffffULL));
-	return ((uint64_t)th << 32) | tl;
+#ifndef bswap32
+	static uint32_t
+	bswap32 (uint32_t x)
+	{
+		return ((x << 24) & 0xff000000) |
+				((x << 8) & 0x00ff0000) |
+				((x >> 8) & 0x0000ff00) |
+				((x >> 24) & 0x000000ff);
+	}
 #endif
-}
+
+#ifndef bswap64
+	static uint64_t
+	bswap64 (uint64_t x)
+	{
+	#ifdef _LP64
+		/*
+		 * Assume we have wide enough registers to do it without touching
+		 * memory.
+		 */
+		return ((x << 56) & 0xff00000000000000UL) |
+				((x << 40) & 0x00ff000000000000UL) |
+				((x << 24) & 0x0000ff0000000000UL) |
+				((x << 8) & 0x000000ff00000000UL) |
+				((x >> 8) & 0x00000000ff000000UL) |
+				((x >> 24) & 0x0000000000ff0000UL) |
+				((x >> 40) & 0x000000000000ff00UL) |
+				((x >> 56) & 0x00000000000000ffUL);
+	#else
+		/*
+		 * Split the operation in two 32bit steps.
+		 */
+		uint32_t tl, th;
+
+		th = bswap32((uint32_t)(x & 0x00000000ffffffffULL));
+		tl = bswap32((uint32_t)((x >> 32) & 0x00000000ffffffffULL));
+		return ((uint64_t)th << 32) | tl;
+	#endif
+	}
+#endif
 
 static char *
 rmilter_encode_base64_common (const u_char *in, size_t inlen, int str_len,
