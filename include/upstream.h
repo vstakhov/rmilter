abe1366fc0bc9d2431ebe1077839a63c56f55f9e
Vsevolod Stakhov <vsevolod@highsecure.ru>
2016-01-14 10:02:36 +0000

    Rework upstreams code

--- include/upstream.h
+++ include/upstream.h
@@ -35,37 +35,27 @@ struct upstream {
 	unsigned char dead;
 	unsigned char priority;
 	int16_t weight;
-	uint32_t *ketama_points;
-	size_t ketama_points_size;
 };
 
 void upstream_fail (struct upstream *up, time_t now);
 void upstream_ok (struct upstream *up, time_t now);
-void revive_all_upstreams (void *ups, size_t members, size_t msize);
-int upstream_ketama_add (struct upstream *up, char *up_key, size_t keylen, size_t keypoints);
+void revive_all_upstreams (void *ups, unsigned int members, unsigned int msize);
 
-struct upstream* get_random_upstream   (void *ups, size_t members, size_t msize, 
-										time_t now, time_t error_timeout, 
-										time_t revive_timeout, size_t max_errors);
+struct upstream* get_random_upstream   (void *ups, unsigned int members, unsigned int msize,
+		time_t now, time_t error_timeout,
+		time_t revive_timeout, unsigned int max_errors);
 
-struct upstream* get_upstream_by_hash  (void *ups, size_t members, size_t msize, 
-										time_t now,  time_t error_timeout, 
-										time_t revive_timeout, size_t max_errors,
-										char *key, size_t keylen);
+struct upstream* get_upstream_by_hash  (void *ups, unsigned int members, unsigned int msize,
+		time_t now,  time_t error_timeout,
+		time_t revive_timeout, unsigned int max_errors,
+		const unsigned char *key, unsigned int keylen);
 
-struct upstream* get_upstream_round_robin (void *ups, size_t members, size_t msize, 
-										time_t now, time_t error_timeout, 
-										time_t revive_timeout, size_t max_errors);
+struct upstream* get_upstream_round_robin (void *ups, unsigned int members, unsigned int msize,
+		time_t now, time_t error_timeout,
+		time_t revive_timeout, unsigned int max_errors);
 
-struct upstream* get_upstream_by_hash_ketama (void *ups, size_t members, size_t msize, time_t now, 
-										time_t error_timeout, time_t revive_timeout, size_t max_errors,
-										char *key, size_t keylen);
-
-struct upstream* get_upstream_master_slave (void *ups, size_t members, size_t msize,
-										time_t now, time_t error_timeout,
-										time_t revive_timeout, size_t max_errors);
+struct upstream* get_upstream_master_slave (void *ups, unsigned int members, unsigned int msize,
+		time_t now, time_t error_timeout,
+		time_t revive_timeout, unsigned int max_errors);
 
 #endif /* UPSTREAM_H */
-/* 
- * vi:ts=4 
- */
